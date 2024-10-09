feda55fb025aecffa5b7b8739da8fda5d07c4912
Manueljcm04 <159296137+Manueljcm04@users.noreply.github.com>
2024-10-09 18:54:18 +0000

    starwar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import logostarwars from "../../img/star wars1.png";
+import "./../../styles/navbar.css";
 
 export const Navbar = () => {
+	const {store,actions} = useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<div>
+			<nav className="navbar navbar-light bg-light mb-3 ">
+            <Link to="/">
+				<span className="navbar-brand mb-0 h1"><img className="imagenDetalle" src={logostarwars}></img></span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+                
+            <div>
+                <div className="btn-group botonFav">
+                    <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Favoritos {store.fav.length}
+                    </button>
+                    <ul className="dropdown-menu">
+                        {store.fav.map((item, index) => (
+                            <li key={index}>
+                                <a className="dropdown-item">{item}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+		</div>
 	);
 };
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,52 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+
+			personajes: [],
+			naves: [],
+			planetas: [],
+			fav: [],
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+
+			//Función para filtrar y mostrar los favoritos en el navbar
+			favorito: (nombreFav) => {
+
 				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				if (store.fav.includes(nombreFav)) {
+					setStore({ fav: store.fav.filter((repetido) => repetido != nombreFav) });
+				}
+				else {
+					setStore({ fav: [...store.fav, nombreFav] });
+				}
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			//Función que comprueba si el array contiene el nombre para luego cambiar el color del corazón
+			corazonColor: name => {
+				const store = getStore();
+				return store.fav.includes(name);
+			},
+
+
+			loadSomeData: () => {
+
+				//Traer a los personajes
+				fetch("https://swapi.dev/api/people")
+					.then((response) => response.json())
+					.then((data) => setStore({ personajes: data.results }))
+
+				//Traer a las naves
+				fetch("https://swapi.dev/api/starships")
+					.then((response) => response.json())
+					.then((data) => setStore({ naves: data.results }))
+
+				//Traer a los planetas
+				fetch("https://swapi.dev/api/planets")
+					.then((response) => response.json())
+					.then((data) => setStore({ planetas: data.results }))
+			},
 		}
 	};
 };
